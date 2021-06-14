ed191ef49ab38f980da268c55d462413b5832878
Arthur Nakagawa de Oliveira <nakagawa0410@hotmail.com>
2021-06-14 11:06:03 -0300

    Fix convert page to string

--- paginated.py
+++ paginated.py
@@ -1,8 +1,8 @@
 def handler_paginated(event, context):
-    print("start_date: " + event["start_date"])
-    print("end_date: " + event["end_date"])
-    print("page" + event["page"])
-    print("is_completed" + event["is_completed"])
+    print("start_date: " + str(event["start_date"]))
+    print("end_date: " + str(event["end_date"]))
+    print("page" + str(event["page"]))
+    print("is_completed" + str(event["is_completed"]))
 
     event["page"] += 1
     if (event["page"] >= 3):
